60b3243db6764a3341d04ef244ce33e3109f271a
Kim Maida <kim.maida@auth0.com>
2016-10-25 18:07:15 -0400

    Consolidate JSON to one file only. Easier to maintain.

--- server.js
+++ server.js
@@ -5,22 +5,32 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var _ = require('lodash');
-var dinos = require('./dinosaurs.json');
-var dinoDetail = require('./dinosaurs-detail.json');
+var dinoJson = require('./dinosaurs.json');
+
+var allDinos = getAllDinos();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/api/dinosaurs', function(req, res) {
-	res.json(dinos);
+	res.json(allDinos);
 });
 
 app.get('/api/dinosaur/:id', function(req,res) {
 	var id = req.params.id * 1;
-	var thisDino = _.find(dinoDetail, { id: id });
+	var thisDino = _.find(dinoJson, { id: id });
 	res.json(thisDino);
 });
+
+function getAllDinos() {
+	return _.map(dinoJson, function(obj) {
+		return {
+			id: obj.id,
+			name: obj.name
+		};
+	});
+}
 
 app.listen(3001);
 console.log('Listening on localhost:3001');
